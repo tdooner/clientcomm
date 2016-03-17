06d17c8a38951c2125fc515c84afc252300a595b
Kuan Butts <kuanbutts@yahoo.com>
2016-03-16 18:04:40 -0700

    moved full nested logic for new msg to utils

--- routes/sms2.js
+++ routes/sms2.js
@@ -4,73 +4,26 @@ var sms = require("../utils/utils.js")["sms"];
 module.exports = function (app, db, passport) {
 
   app.post("/test", function (req, res) {
-    console.log(req.body)
-  });
-
-  app.post("/sms", function (req, res) {
 
     var twiml = new twilio.TwimlResponse();
     var from = sms.clean_from_val(req.body.From);
     var text = req.body.Body.trim();
-    var tw_sid = req.body.SmsSid;
+
     var tw_status = req.body.SmsStatus;
+    var tw_sid = req.body.MessageSid;
 
-    sms.get_or_create_comm_device(from)
-    .then(function (device) {
-
-      // if this communication device exists in the system already
-      if (device.length > 0) {
-        device = device[0];
-        var commid = device.commid;
-
-        // get clients
-        sms.get_clients(commid)
-        .then(function (clients) {
-
-          // at least one client should be returned
-          if (clients.length > 0) {
-            sms.get_or_create_convos(clients)
-            .then(function (convos) {
-
-              // need to make sure that there are existing convos
-              if (convos.length > 0) {
-                sms.register_message(text, commid, convos)
-                .then(function (msgs) {
-
-
-                }).catch(function (err) {
-                  handleError(err);
-                });
-
-              // convos list should have been returned
-              } else {
-                handleError("Failed to produce convos list.");
-              }
-
-            }).catch(function (err) {
-              handleError(err);
-            });
-
-          // a client or null val should have been returned
-          } else {
-            handleError("Failed to produce client or null list value.");
-          }
-
-        }).catch(function (err) {
-          handleError(err);
-        });
-
-      // that number does not currently exist
-      } else {
-        handleError("Failed to create a comm device.")
-      } 
+    sms.process_incoming_msg(from, text, tw_status, tw_sid)
+    .then(function (msgs) {
+      var msg = "Succ.";
+      twiml.sms(msg);
+      res.send(twiml.toString());
     }).catch(function (err) {
       handleError(err);
-    });
+    })
 
     function handleError (err) {
-      var now = new Date(Date.now()).toISOString()
-      console.log("Error occurred at " + now + ": " + err);
+      var now = new Date(Date.now()).toISOString().split("T");
+      console.log("Error occurred on " + now[0] + " at " + now[1] + ": " + err);
       res.status(404).send(err);
       return false;
     };
--- utils/sms.js
+++ utils/sms.js
@@ -1,11 +1,67 @@
-// var db  = require("../server/db");
+var db  = require("../server/db");
 var Promise = require("bluebird");
 
 module.exports = {
 
+	process_incoming_msg: function (from, text, tw_status, tw_sid) {
+		var that = this;
+		return new Promise (function (fulfill, reject) {
+	    that.get_or_create_comm_device(from)
+	    .then(function (device) {
+
+	      // if this communication device exists in the system already
+	      if (device.length > 0) {
+	        var commid = device[0];
+
+	        // get clients
+	        that.get_clients(commid)
+	        .then(function (clients) {
+
+	          // at least one client should be returned
+	          if (clients.length > 0) {
+	            that.get_or_create_convos(clients, from)
+	            .then(function (convos) {
+	              // need to make sure that there are existing convos
+	              if (convos.length > 0) {
+	                that.register_message(text, commid, convos, tw_status, tw_sid)
+	                .then(function (msgs) {
+	                	fulfill(msgs);
+
+	                }).catch(function (err) {
+	                  reject(err);
+	                });
+
+	              // convos list should have been returned
+	              } else {
+	                reject("Failed to produce convos list.");
+	              }
+
+	            }).catch(function (err) {
+	              reject(err);
+	            });
+
+	          // a client or null val should have been returned
+	          } else {
+	            reject("Failed to produce client or null list value.");
+	          }
+
+	        }).catch(function (err) {
+	          reject(err);
+	        });
+
+	      // that number does not currently exist
+	      } else {
+	        reject("Failed to create a comm device.")
+	      } 
+	    }).catch(function (err) {
+	      reject(err);
+	    });
+		});
+	},
+
 	clean_from_val: function (from) {
 		if (from) {
-			from.replace(/\D+/g, "");
+			from = from.replace(/\D+/g, "");
 			if (from.length == 10) {
 				from = "1" + from;
 			}
@@ -24,12 +80,13 @@ module.exports = {
 	get_or_create_comm_device: function (from) {
 		return new Promise (function (fulfill, reject) {
 		  // acquire commid
-		  db("comms").where("value", from).limit(1)
-		  .then(function (comm) {
+		  db.select("commid").from("comms").where("value", from).limit(1)
+		  .then(function (comms) {
 		  	
 		  	// return list of comms
-		  	if (comm.length > 0) {
-		  		fulfill(comm);
+		  	if (comms.length > 0) {
+		    	comms = comms.map(function (ea) { return ea.commid; });
+		  		fulfill(comms);
 
 		  	// create and return a comm
 		  	} else {
@@ -85,29 +142,50 @@ module.exports = {
 	    	if (convos.length > 0) {
 	    		fulfill(convos);
 
-	    	// we need to create new conversation(s)
+	    	// we need to check if there is an unlinked convo associated to this 
 	    	} else {
 
-	    		// just in case [null] value was not submitted for clients in lieu of none
-	    		if (clients.length == 0) { clients = [null]; }
-
-		    	var insertList = [];
-		    	for (var i = 0; i < clients.length; i++) {
-		    		var client = clients[i];
-		    		var insertObj = {
-		    			"cm": null,
-		    			"client": client,
-		    			"open": true,
-		    			"accepted": false
-		    		}
-		    		insertList.push(insertObj);
-		    	}
-
-			    db("convos")
-			    .insert(insertList)
-			    .returning("convid")
+	    		db.select("convos.convid").from("comms")
+	    		.innerJoin("msgs", "comms.commid", "msgs.comm")
+	    		.innerJoin("convos", "msgs.convo", "convos.convid")
+	    		.where("convos.open", true)
+	    		.groupBy("convos.convid")
 			    .then(function (convos) {
-			    	fulfill(convos);
+
+			    	// clean up response
+			    	convos = convos.map(function (ea) { return ea.convid; });
+
+			    	// there are existing open conversations
+			    	if (convos.length > 0) {
+			    		fulfill(convos);
+
+			    	} else {
+
+			    		// just in case [null] value was not submitted for clients in lieu of none
+			    		if (clients.length == 0) { clients = [null]; }
+
+				    	var insertList = [];
+				    	for (var i = 0; i < clients.length; i++) {
+				    		var client = clients[i];
+				    		var insertObj = {
+				    			"cm": null,
+				    			"client": client,
+				    			"open": true,
+				    			"accepted": false
+				    		}
+				    		insertList.push(insertObj);
+				    	}
+
+					    db("convos")
+					    .insert(insertList)
+					    .returning("convid")
+					    .then(function (convos) {
+					    	fulfill(convos);
+					    }).catch(function (err) {
+							  reject(err);
+							});
+						}
+
 			    }).catch(function (err) {
 					  reject(err);
 					});
@@ -119,7 +197,7 @@ module.exports = {
 		});
 	},
 
-	register_message: function (text, commid, convos) {
+	register_message: function (text, commid, convos, tw_status, tw_sid) {
     return new Promise (function (fulfill, reject) {
     	var insertList = [];
     	for (var i = 0; i < convos.length; i++) {
@@ -129,16 +207,18 @@ module.exports = {
     			"comm": commid,
     			"content": text,
     			"inbound": true,
-    			"read": false
+    			"read": false,
+    			"tw_sid": tw_sid,
+    			"tw_status": tw_status
     		}
     		insertList.push(insertObj);
     	}
 
 	    db("msgs")
 	    .insert(insertList)
-	    .returning("convid")
-	    .then(function (convos) {
-	    	fulfill(convos);
+	    .returning("msgid")
+	    .then(function (msgs) {
+	    	fulfill(msgs);
 	    }).catch(function (err) {
 			  reject(err);
 			});
