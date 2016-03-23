a54e4a211720d4deb0c12a1737d251545d79b8c2
Kuan Butts <kuanbutts@yahoo.com>
2016-03-23 09:24:18 -0600

    insert convid

--- utils/sms.js
+++ utils/sms.js
@@ -200,7 +200,7 @@ module.exports = {
     		}
     		var convo = convos[i];
     		var insertObj = {
-    			"convo": convo,
+    			"convo": convo.convid,
     			"comm": commid,
     			"content": text,
     			"inbound": true,
