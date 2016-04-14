d420b4d978996e8f364d9c72ae90996acd6099a5
Kuan Butts <kuanbutts@yahoo.com>
2016-04-14 09:13:57 -0700

    admins can create new users and view them without 500s

--- routes/admin.js
+++ routes/admin.js
@@ -57,6 +57,85 @@ router.get("/", function (req, res) {
 	}).catch(function (err) { res.redirect("/500"); });
 });
 
+router.post("/", function (req, res) {
+	var redirect_loc = "/admin";
+
+	var orgid = Number(req.body.orgid);
+	var first = req.body.first;
+	var middle = req.body.middle;
+	var last = req.body.last;
+	var email = req.body.email;
+	var password = req.body.password;
+	var position = req.body.position;
+	var department = req.body.department;
+	var admin = req.body.admin;
+
+	if (typeof email == "string") { email = email.toLowerCase(); }
+
+	if (admin == "true") admin = true;
+	if (admin !== true) admin = false;
+	if (!middle) middle = null;
+
+	if (!orgid) {
+		req.flash("warning", "Missing orgid.");
+		res.redirect(redirect_loc);
+	} else if (!first) {
+		req.flash("warning", "Missing first name.");
+		res.redirect(redirect_loc);
+	} else if (!last) {
+		req.flash("warning", "Missing last name.");
+		res.redirect(redirect_loc);
+	} else if (!email) {
+		req.flash("warning", "Missing email.");
+		res.redirect(redirect_loc);
+	} else if (!password) {
+		req.flash("warning", "Missing password.");
+		res.redirect(redirect_loc);
+	} else if (!position) {
+		req.flash("warning", "Missing position.");
+		res.redirect(redirect_loc);
+	} else if (!department) {
+		req.flash("warning", "Missing department.");
+		res.redirect(redirect_loc);
+	} else if (Number(req.user.org) !== orgid) {
+		req.flash("warning", "Mismatched organization id's on form submit.");
+		res.redirect(redirect_loc);
+	} else {
+
+		db("cms").where("email", email)
+		.then(function (cms) {
+
+			if (cms.length > 0) {
+				req.flash("warning", "Email already in use.");
+				res.redirect(redirect_loc);
+			} else {
+				db("cms").insert({
+					org: orgid,
+					first: first,
+					middle: middle,
+					last: last,
+					email: email,
+					pass: pass.hashPw(password),
+					position: position,
+					department: department,
+					admin: admin,
+					active: true,
+					superuser: false,
+				}).then(function (success) {
+					req.flash("success", "Added new member.");
+					res.redirect(redirect_loc);
+				}).catch(function () {
+					res.redirect("/500");
+				});
+			}
+
+		}).catch(function () {
+			res.redirect("/500");
+		})
+
+	}
+});
+
 router.get("/cms", function (req, res) {
 	res.redirect("/admin");
 })
@@ -73,14 +152,19 @@ router.get("/cms/:cmid", function (req, res) {
 
     } else if ((req.user.cmid !== cmid) && (req.user.org == cms[0].org)) {
       var cm = cms[0];
-
       db("convos").where("cm", cmid).pluck("convid")
       .then(function (convos) {
+
+      	if (convos.length == 0) { convos = ["null"]; };
+
       	var rawQuery = "SELECT COUNT(*), convo, date(msgs.created) FROM msgs INNER JOIN convos ON (convos.convid=msgs.convo) WHERE convos.convid IN (";
       	rawQuery += convos.join(", ");
-      	rawQuery += ") GROUP BY convo, date(msgs.created) ORDER BY DATE DESC"
+      	rawQuery += ") GROUP BY convo, date(msgs.created) ORDER BY DATE DESC";
+      	if (convos[0] == "null") { rawQuery += " LIMIT 0"; };
 	      db.raw(rawQuery)
-	      .then(function (msgs) {
+	      .then(function (msgs) {console.log(rawQuery);console.log(msgs);
+
+	      	if (convos[0] == "null") { msgs.rows = []; };
 
 	        res.render("clientstats", {
 	          cm: cm,
