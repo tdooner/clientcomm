f69c16efe0a68d6d251cc9d2cfe855f1cf9ec9a5
Kuan Butts <kuanbutts@yahoo.com>
2016-05-25 17:00:43 -0700

    migrations for timezone var

--- migrations/20160405162146_dob_col_typechange.js
+++ migrations/20160405162146_dob_col_typechange.js
@@ -1,13 +1,18 @@
 
 exports.up = function(knex, Promise) {
 	return Promise.all([
-		// alter dob to datetime
+
+		// Alter dob col type to datetime
 		knex.raw("ALTER TABLE clients ALTER COLUMN dob TYPE TIMESTAMP WITH TIME ZONE USING to_timestamp(dob, 'YYYY-MM-DD HH24:MI:SS')"),
+
 	])
 };
 
 exports.down = function(knex, Promise) {
 	return Promise.all([
+
+		// Return dob col type to string
 		knex.raw("ALTER TABLE clients ALTER COLUMN dob TYPE varchar(255)"),
+
 	])
 };
