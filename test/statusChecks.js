26443715cf4433cf580565a323869faadafd88ee
Kuan Butts <kuanbutts@yahoo.com>
2016-05-29 10:52:08 -0700

    testing routes for login logic

--- test/statusChecks.js
+++ test/statusChecks.js
@@ -1,10 +1,10 @@
-var assert = require('chai').assert;
+var assert = require("chai").assert;
 
 // Makes sure that testing is functioning okay
 // If this fails, then everything else is likely failing as well
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
+describe("Array", function() {
+  describe("#indexOf()", function () {
+    it("Should return -1 when the value is not present", function () {
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     });
@@ -12,34 +12,50 @@ describe('Array', function() {
 });
 
 
-var request = require('supertest');
+// Load up Supertest package
+var request = require("supertest");
 
+
+// Work through the routes
 describe("Boot up express.", function () {
   var server;
   
+  // Start up the server each time (does this mean we have to login each time?)
   beforeEach(function () {
-    server = require('../server/app');
+    server = require("../server/app");
   });
   
   afterEach(function () {
     server.close();
   });
   
-  it('responds to /', function testSlash(done) {
-  
-  request(server)
-    .get('/')
-    .expect(302, done);
+  it("Index page", function (done) {
+    request(server).get("/")
+      .expect(302, done);
   });
   
-  it('404 everything else', function testPath(done) {
-    request(server)
-      .get('/foo/bar')
+  it("Login page", function (done) {
+    request(server).get("/login")
+      .expect(200, done);
+  });
+  
+  it("POST to login page", function (done) {
+    request(server).post("/login")
+      .field("email", "tEsT@foo.com")
+      .field("pass", "123")
+      .expect(302, done);
+  });
+  
+  it("POST to login page (V2 - Email must not be case sensitive)", function (done) {
+    request(server).post("/login")
+      .field("email", "TEST@foo.com")
+      .field("pass", "123")
+      .expect(302, done);
+  });
+  
+  it("All unknown paths 404", function (done) {
+    request(server).get("/foo/bar")
       .expect(404, done);
   });
 
-  it('protects against sql injection on the edit to commconn route', function testSqlInjectionInEditCommConn() {
-  	request(server)
-  		.post('/14/cls/6/comms/22')
-  });
 });
