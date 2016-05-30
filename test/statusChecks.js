86c7c0609a54f2e1d57f80eff137dc701be899db
Kuan Butts <kuanbutts@yahoo.com>
2016-05-30 16:39:32 -0700

    tests abstraction contd, cleaning

--- test/statusChecks.js
+++ test/statusChecks.js
@@ -61,18 +61,10 @@ function runTests () {
     it("Login as super user and create an org", function (done) {
       // Create server request object
       var req = request(server)
-
       // P1: First login as superuser
       u.superuserLogin(req, function () {
-
       // P2: POST a new organization
-      req.post("/orgs")
-        .field("name", "Example Organization")
-        .field("phone", "18008008000")
-        .field("email", "fooorg@foo.com")
-        .field("expiration", "2020-01-01")
-        .field("allotment", "10000")
-        .expect(302, done);
+      u.createOrganization(req, done);
 
       }); // P1
     });
@@ -80,31 +72,12 @@ function runTests () {
     it("Create a supervisor for an organization", function (done) {
       // Create server request object
       var req = request(server)
-
       // P1: First login as superuser
       u.superuserLogin(req, function () {
-
       // P2: POST a new organization
-      req.post("/orgs")
-        .field("name", "Example Organization")
-        .field("phone", "18008008000")
-        .field("email", "fooorg@foo.com")
-        .field("expiration", "2020-01-01")
-        .field("allotment", "10000")
-        .expect(302, function() {
-
+      u.createOrganization(req, function() {
       // P3: Create first supervisor for organization
-      req.post("/orgs/1")
-        .field("orgid", "1")
-        .field("first", "Jim")
-        .field("middle", "M")
-        .field("last", "Surie")
-        .field("email", "jim@foo.com")
-        .field("password", "123")
-        .field("position", "Supervisor")
-        .field("department", "Pretrial")
-        .field("admin", "true")
-        .expect(302, done);
+      u.createSupervisor(req, done);
         
       }); // P2
       }); // P1
@@ -113,67 +86,33 @@ function runTests () {
     it("Successful session logout", function (done) {
       // Create server request object
       var req = request(server)
-
       // P1: First login as superuser
       u.superuserLogin(req, function () {
-
       // P2: Logout
-      req.get("/logout")
-        .expect(302, done);
+      u.logout(req, done);
 
       }); // P1
     });
 
-    it("POST to login page", function (done) {
+    it("Login as test user", function (done) {
       // Create server request object
       var req = request(server)
 
       // P1: First login as superuser
       u.superuserLogin(req, function () {
-
       // P2: POST a new organization
-      req.post("/orgs")
-        .field("name", "Example Organization")
-        .field("phone", "18008008000")
-        .field("email", "fooorg@foo.com")
-        .field("expiration", "2020-01-01")
-        .field("allotment", "10000")
-        .expect(302, function() {
-
+      u.createOrganization(req, function() {
       // P3: Create first supervisor for organization
-      req.post("/orgs/1")
-        .field("orgid", "1")
-        .field("first", "Jim")
-        .field("middle", "M")
-        .field("last", "Surie")
-        .field("email", "jim@foo.com")
-        .field("password", "123")
-        .field("position", "Supervisor")
-        .field("department", "Pretrial")
-        .field("admin", "true")
-        .expect(302, function () {
-
+      u.createSupervisor(req, function () {
       // P4: Logout
-      req.get("/logout")
-        .expect(302, function () {
-
+      u.logout(req, function () {
       // P5: Login as the new user
-      req.post("/login")
-        .field("email", "jim@foo.com")
-        .field("pass", "123")
-        .expect(302, done);
+      u.testuserLogin(req, done);
 
       }); // P4
       }); // P3
       }); // P2
       }); // P1
-    });
-    
-    it("POST to login page (V2 - Email must not be case sensitive)", function (done) {
-      request(server).post("/login")
-        .field("email", "JiM@foo.com")
-        .field("pass", "123")
-        .expect(302, done);
     });
     
     it("Be rerouted to splash page when already logged in", function (done) {
--- test/utils.js
+++ test/utils.js
@@ -9,11 +9,48 @@ var creds = require("../credentials");
 
 
 module.exports = {
+  
   superuserLogin: function (req, cb) {
-    console.log("working");
     req.post("/login")
       .field("email", creds.db.user)
       .field("pass", creds.db.password)
       .expect(302, cb);
+  },
+
+  createOrganization: function (req, cb) {
+    req.post("/orgs")
+      .field("name", "Example Organization")
+      .field("phone", "18008008000")
+      .field("email", "fooorg@foo.com")
+      .field("expiration", "2020-01-01")
+      .field("allotment", "10000")
+      .expect(302, cb);
+  },
+
+  createSupervisor: function (req, cb) {
+    req.post("/orgs/1")
+      .field("orgid", "1")
+      .field("first", "Jim")
+      .field("middle", "M")
+      .field("last", "Surie")
+      .field("email", "jim@foo.com")
+      .field("password", "123")
+      .field("position", "Supervisor")
+      .field("department", "Pretrial")
+      .field("admin", "true")
+      .expect(302, cb);
+  },
+
+  testuserLogin: function (req, cb) {
+    req.post("/login")
+      .field("email", "jim@foo.com")
+      .field("pass", "123")
+      .expect(302, cb);
+  },
+
+  logout: function (req, cb) {
+      req.get("/logout")
+        .expect(302, cb);
   }
-}
+
+};
