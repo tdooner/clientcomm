60d188863d413cc3d4bd776b26b220f17f46a08e
Kuan Butts <kuanbutts@yahoo.com>
2016-06-01 13:20:01 -0700

    updated error util use on routes in cmview

--- utils/error-handlers.js
+++ utils/error-handlers.js
@@ -6,10 +6,12 @@ module.exports = {
 	fivehundred: function (res) { 
 		return function (err) {
 			
-			// Log the error
-			console.log("Error occured. \n Timestampt: " + new Date());
-			console.log(err);
-			console.log("---");
+			// Log the error if passed in
+			if (typeof err !== "undefined") {
+				console.log("Error occured. \n Timestampt: " + new Date());
+				console.log(err);
+				console.log("---");
+			}
 
 			// Run the redirect
 			res.redirect("/500"); 
