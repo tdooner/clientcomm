b08a678c2285606f85b33d60d86ac898f1506fb1
Kuan Butts <kuanbutts@yahoo.com>
2016-06-24 14:36:58 -0700

    first pass at notification migration logic

---
+++ migrations/20160624140642_notifications_table.js
@@ -0,0 +1,47 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+
+    knex.schema.createTable("notifications", function(table) {
+      table.increments("notificationid").primary();
+
+      // Foreign keys
+      table.integer("cm")
+           .references("cmid")
+           .inTable("cms");
+
+      table.integer("client")
+           .references("clid")
+           .inTable("clients");
+
+      table.integer("comm")
+           .references("commid")
+           .inTable("comms");
+
+      // Content
+      table.string("subject");
+      table.text("message");
+
+      // Time related content
+      table.timestamp("created").defaultTo(knex.fn.now());
+      table.timestamp("send");
+
+      // Repeat options
+      table.boolean("repeat").defaultTo(false);
+      table.integer("frequency");
+
+      // Book keeping
+      table.boolean("sent").defaultTo(false);
+    })
+
+
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+
+    knex.schema.dropTable("notifications")
+
+  ]);
+};
