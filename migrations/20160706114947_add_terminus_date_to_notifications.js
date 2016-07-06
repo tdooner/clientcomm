31fcbbaaa8a8488ffec9db3f36ed0872e2fcb6b9
Kuan Butts <kuanbutts@yahoo.com>
2016-07-06 11:57:12 -0700

    add terminus to repeat on notifications

---
+++ migrations/20160706114947_add_terminus_date_to_notifications.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+
+    // Create a new col for notifications that holds terminus date for repeat
+    knex.raw("ALTER TABLE notifications ADD COLUMN repeat_terminus TIMESTAMPTZ;"),
+
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+
+    // Remove col from notifications that holds terminus date for repeat
+    knex.raw("ALTER TABLE notifications DROP COLUMN IF EXISTS repeat_terminus;"),
+
+  ])
+};
