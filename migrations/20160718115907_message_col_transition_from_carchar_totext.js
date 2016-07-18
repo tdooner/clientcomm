860fc35200cfb07afb01b6536de3236022f5f4d0
Kuan Butts <kuanbutts@yahoo.com>
2016-07-18 13:20:51 -0700

    resovles issue 90, varchar to text migration

---
+++ migrations/20160718115907_message_col_transition_from_carchar_totext.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+
+    // Alter msgs col type to text cell
+    knex.raw("ALTER TABLE msgs ALTER COLUMN content TYPE text"),
+
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+
+    // Return msgs col type to varchar cell length 255
+    knex.raw("ALTER TABLE msgs ALTER COLUMN content TYPE varchar(255)"),
+
+  ])
+};
+
