cd87e557b7321e47c8f5492f34c1a14461fe4fed
Kuan Butts <kuanbutts@yahoo.com>
2016-07-20 16:20:53 -0700

    migrations for tempaltes

---
+++ migrations/20160720154931_templates.js
@@ -0,0 +1,35 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+
+    knex.schema.createTable("templates", function(table) {
+      table.increments("template_id").primary();
+
+      table.integer("org")
+           .references("orgid")
+           .inTable("orgs");
+
+      table.integer("cm")
+           .references("cmid")
+           .inTable("cms");
+
+      table.integer("cl")
+           .references("clid")
+           .inTable("clients");
+
+      table.text("content");
+      
+      table.timestamp("created").defaultTo(knex.fn.now());
+    })
+
+
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+
+    knex.schema.dropTable("client_closeout_surveys")
+
+  ]);
+};
