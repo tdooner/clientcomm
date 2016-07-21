bbd9b5b1f73e89211059e73d7a57e35699628418
Kuan Butts <kuanbutts@yahoo.com>
2016-07-21 15:42:24 -0700

    mod migration to have default color ALTER TABLE clients ADD COLUMN color_tag varchar(255) DEFAULT '#898989';

--- migrations/20160721151811_clients_color_tag.js
+++ migrations/20160721151811_clients_color_tag.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
 
-    knex.raw("ALTER TABLE clients ADD COLUMN color_tag varchar(255);"),
+    knex.raw("ALTER TABLE clients ADD COLUMN color_tag varchar(255) DEFAULT '#898989';"),
 
   ])
 };
