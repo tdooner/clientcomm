c0c2da17975ee5b4fd48ec330852d7d978bf0810
Kuan Butts <kuanbutts@yahoo.com>
2016-07-25 09:53:57 -0700

    migrate the templates table to have col for title

---
+++ migrations/20160725095224_template_add_subject_col.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+
+    knex.raw("ALTER TABLE templates ADD COLUMN title varchar(255);"),
+
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+
+    knex.raw("ALTER TABLE templates DROP COLUMN IF EXISTS title;"),
+
+  ])
+};
