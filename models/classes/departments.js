8f61b2260289fdd983eb5c9375bfa35e65fc6256
Kuan Butts <kuanbutts@yahoo.com>
2016-08-23 23:27:32 -0700

    rendering and querying for all departments with association variables

--- models/classes/departments.js
+++ models/classes/departments.js
@@ -18,9 +18,23 @@ class Departments {
     if (typeof activeStatus == "undefined") activeStatus = true;
     return new Promise((fulfill, reject) => {
       db("departments")
-        .where("organization", orgID)
-        .andWhere("active", activeStatus)
-        .orderBy("name", "asc")
+        .select("departments.*", 
+                "phone_numbers.value", 
+                "member_counts.members",
+                "cms.first as created_by_first",
+                "cms.middle as created_by_middle",
+                "cms.last as created_by_last")
+        .leftJoin(
+          db("cms")
+            .select(db.raw("COUNT(*) AS members"), "department")
+            .groupBy("department")
+            .as("member_counts"),
+          "member_counts.department", "departments.department_id")
+        .leftJoin("cms", "cms.cmid", "departments.created_by")
+        .leftJoin("phone_numbers", "phone_numbers.phone_number_id", "departments.phone_number")
+        .where("departments.organization", orgID)
+        .andWhere("departments.active", activeStatus)
+        .orderBy("departments.name", "asc")
       .then((departments) => {
         fulfill(departments)
       }).catch(reject);
