50f7dd473fce924c7b6d6f89fecd64ec1a2c01aa
Kuan Butts <kuanbutts@yahoo.com>
2016-08-26 14:34:01 -0700

    when using testing env still use amazon rds so that staging can be possible

--- server/db.js
+++ server/db.js
@@ -1,4 +1,10 @@
-var env = "development";
+var env = "production";
+
+// Update environment to use testing tools when applicable
+var TESTENV = process.env.TESTENV;
+if (TESTENV && TESTENV == "true") {
+  env = "development";
+}
 
 var config = require('../knexfile');
 var knex = require("knex")(config[env]);
