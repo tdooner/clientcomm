786107ed9e4fa7d324d7540140df2db68cfcaec0
maxmcd <max.t.mcdonnell@gmail.com>
2016-09-06 21:31:18 -0700

    better errors and 404 page

--- routes/versionFour/utilities/errorHandling.js
+++ routes/versionFour/utilities/errorHandling.js
@@ -1,29 +1,30 @@
+const colors = require('colors')
+
 module.exports = {
 
   error_500: function (res) { 
     // In the future, note that this could cause a performance issue if product on non-human actions
     return function (err) {
-
       // Clean up error if one is provided
       if (typeof err !== "undefined") {
-        // Convert the error to a string in case it is an object
-        err = JSON.stringify(err);
+
         // Log the error if passed in
-        console.log("\n Error occured. \n Timestamp: " + new Date());
-        console.log(err);
+        console.log(`\n Error occured. \n Timestamp: ${new Date()}`.yellow);
+        console.log(err.stack);
         console.log("--- \n");
 
       // If there is no error, provide a generic phrase
       } else {
-        err = "Internal Error 500 Something happened.";
+        stringErr = "Internal Error 500 Something happened.";
       }
 
       // Produce a response to the client
-      res.status(500).send(String(err))
+      res.set({'content-type':'text/plain'}).status(500).send(err.stack)
+
     }
   },
   notFound: (res) => {
-    res.status(404).send('v4/general/404')
+    res.status(404).render('v4/general/404')
   } 
 
 }
