1a316c3ffd3d5f093d5fee0741d2113f69045bf8
Kuan Butts <kuanbutts@yahoo.com>
2016-09-14 14:46:30 -0700

    #161 branch further changes from call this am with max, involving increased testing and package mods with removed dependencies, all future commits will be on new branches, will merge this in to v4 and close out this branch

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -15,6 +15,8 @@ exports.seed = function(knex, Promise) {
       return knex.migrate.latest()
     }).then(() => {
       console.log("Inserting seed data".yellow)
+      return knex('phone_numbers').insert(phoneNumber)
+    }).then(() => {
       return knex('orgs').insert(org)
     }).then(() => {
       return knex('cms').insert(owner)
@@ -30,49 +32,56 @@ exports.seed = function(knex, Promise) {
   }
 }
 
+let phoneNumber = {
+  // phone_number_id: 1,
+  organization: 1,
+  value: 12435678910,
+}
+
 let org = {
-    orgid: 1, 
-    name: 'Example CJS',
-    email: "test@test.com",
-    expiration: "2018-01-01 00:00:00+00",
-    allotment: 10,
-    created: "2016-03-23 07:05:49.381857+00",
-    tz: "America/Denver",
+  // orgid: 1, 
+  name: 'Example CJS',
+  email: "test@test.com",
+  expiration: "2018-01-01 00:00:00+00",
+  allotment: 10,
+  created: "2016-03-23 07:05:49.381857+00",
+  tz: "America/Denver",
 }
 
 let owner = {
-    cmid: 1,
-    org: 1,
-    first: "Test Account",
-    last: "To Remove",
-    email: "owner@test.com",
-    pass: "$2a$08$LU2c2G3e1L/57JSP3q/Ukuz1av2DXmj6oDUgmNWmAdxTPG5aA/gti", //123
-    position: "Officer",
-    admin: false,
-    active: true,
-    superuser: false,
-    class: "owner",
+  // cmid: 1,
+  org: 1,
+  first: "Test Account",
+  last: "To Remove",
+  email: "owner@test.com",
+  pass: "$2a$08$LU2c2G3e1L/57JSP3q/Ukuz1av2DXmj6oDUgmNWmAdxTPG5aA/gti", //123
+  position: "Officer",
+  admin: false,
+  active: true,
+  superuser: false,
+  class: "owner",
 }
 
 let dep = {
-    department_id: 1,
-    organization: 1,
-    name: "Pretrial",
-    created_by: 1,
-    active: true,
+  // department_id: 1,
+  organization: 1,
+  name: "Pretrial LKJKLJUnique",
+  phone_number: 1,
+  created_by: 1,
+  active: true,
 }
 
 let primary = {
-    cmid: 2,
-    org: 1,
-    first: "Test Account",
-    last: "To Remove",
-    email: "primaryy@test.com",
-    pass: "$2a$08$LU2c2G3e1L/57JSP3q/Ukuz1av2DXmj6oDUgmNWmAdxTPG5aA/gti", //123
-    position: "Officer",
-    admin: false,
-    active: true,
-    superuser: false,
-    class: "primary",
-    department: 1,
+  // cmid: 2,
+  org: 1,
+  first: "Test Account",
+  last: "To Remove",
+  email: "primary@test.com",
+  pass: "$2a$08$LU2c2G3e1L/57JSP3q/Ukuz1av2DXmj6oDUgmNWmAdxTPG5aA/gti", //123
+  position: "Officer",
+  admin: false,
+  active: true,
+  superuser: false,
+  class: "primary",
+  department: 1,
 }
