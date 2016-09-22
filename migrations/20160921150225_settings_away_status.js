20c895ffe4ba5216c9a94cc5e186207ac930fb32
Kuan Butts <kuanbutts@yahoo.com>
2016-09-21 19:04:06 -0700

    #155 migrations typo

--- migrations/20160921150225_settings_away_status.js
+++ migrations/20160921150225_settings_away_status.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
 
     // Create a new col for timezone
     knex.raw(`
-              ALTER TABLE templates 
+              ALTER TABLE cms 
                 ADD COLUMN is_away BOOLEAN default FALSE,
                 ADD COLUMN away_message TEXT default 'I am currently out of office. I will respond as soon as possible.';
             `),
@@ -17,7 +17,7 @@ exports.down = function(knex, Promise) {
 
     // Remove the timezone column, you will lose that data
     knex.raw(`
-              ALTER TABLE templates 
+              ALTER TABLE cms 
                 DROP COLUMN IF EXISTS is_away,
                 DROP COLUMN IF EXISTS away_message;
             `),
