42174e0a22a4338ed9cbfa1d4dcc8d10cfbc0de6
maxmcd <max.t.mcdonnell@gmail.com>
2016-09-26 16:01:10 -0400

    #184 attachments model and migration, s3 lib and requirements

---
+++ migrations/20160926143118_create_attachments.js
@@ -0,0 +1,27 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable("attachments", function(table) {
+
+      table.increments("id").primary();
+
+      table.string("key");
+      table.string("contentType");
+
+      table.integer("msg_id")
+        .references("msgid")
+        .inTable("msgs")
+
+      table.timestamp("created").defaultTo(knex.fn.now());
+
+    }),
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+
+    knex.schema.dropTable("attachments"),
+
+  ]);
+};
