5c3853e2bd39cf08b3fd68270f2b8dd8fd06c84e
Kuan Butts <kuanbutts@yahoo.com>
2016-10-13 22:06:26 -0600

    splash site does not allow actual use and just routes to secure

--- app/app.js
+++ app/app.js
@@ -96,175 +96,175 @@ app.get('/', RootController.index);
 
 // only on instance dedicated to receive, operations
 
-app.post('/webhook/sms', SmsController.webhook);
-app.post('/webhook/voice', VoiceController.webhook);
-app.post('/webhook/voice/status', VoiceController.status);
-app.post('/webhook/voice/transcribe', VoiceController.transcribe);
-app.post('/webhook/voice/record', VoiceController.record);
-app.post('/webhook/voice/save-recording', VoiceController.save);
-app.post('/webhook/voice/play-message', VoiceController.playMessage);
-app.post('/webhook/email', EmailsController.webhook);
-app.post('/webhook/email/status', EmailsController.status);
+// app.post('/webhook/sms', SmsController.webhook);
+// app.post('/webhook/voice', VoiceController.webhook);
+// app.post('/webhook/voice/status', VoiceController.status);
+// app.post('/webhook/voice/transcribe', VoiceController.transcribe);
+// app.post('/webhook/voice/record', VoiceController.record);
+// app.post('/webhook/voice/save-recording', VoiceController.save);
+// app.post('/webhook/voice/play-message', VoiceController.playMessage);
+// app.post('/webhook/email', EmailsController.webhook);
+// app.post('/webhook/email/status', EmailsController.status);
 
-// otherwise this is a user facing implementation
+// // otherwise this is a user facing implementation
 
-app.get('/login', AccessController.login);
-app.post('/login', passport.authenticate('local-login', {
-  successRedirect: '/',
-  failureRedirect: '/login-fail',
-})
-);
-app.get('/login-fail', AccessController.loginFail);
-app.get('/login/reset', AccessController.reset);
-app.post('/login/reset', AccessController.resetSubmit);
-app.get('/login/reset/:uid', AccessController.resetSpecific);
-app.post('/login/reset/:uid', AccessController.resetSpecficSubmit);
+// app.get('/login', AccessController.login);
+// app.post('/login', passport.authenticate('local-login', {
+//   successRedirect: '/',
+//   failureRedirect: '/login-fail',
+// })
+// );
+// app.get('/login-fail', AccessController.loginFail);
+// app.get('/login/reset', AccessController.reset);
+// app.post('/login/reset', AccessController.resetSubmit);
+// app.get('/login/reset/:uid', AccessController.resetSpecific);
+// app.post('/login/reset/:uid', AccessController.resetSpecficSubmit);
 
-// Everything below this, you must be logged in
-app.use(auth.isLoggedIn);
-app.use(auth.checkIsAllowed);
+// // Everything below this, you must be logged in
+// app.use(auth.isLoggedIn);
+// app.use(auth.checkIsAllowed);
 
-app.get('/logout', AccessController.logout);
+// app.get('/logout', AccessController.logout);
 
-app.get('/help', HelpController.index);
+// app.get('/help', HelpController.index);
 
-app.get('/alerts', AlertsController.checkForNewMessages);
-app.get('/alerts/:alert/close', AlertsController.close);
+// app.get('/alerts', AlertsController.checkForNewMessages);
+// app.get('/alerts/:alert/close', AlertsController.close);
 
-app.get('/colors', ColorsController.index);
-app.post('/colors', ColorsController.create);
-app.get('/colors/:color/remove', ColorsController.remove);
+// app.get('/colors', ColorsController.index);
+// app.post('/colors', ColorsController.create);
+// app.get('/colors/:color/remove', ColorsController.remove);
 
-app.get('/notifications', NotificationsController.index);
-app.get('/notifications/create', NotificationsController.new);
-app.get('/notifications/create/compose', NotificationsController.compose);
-app.post('/notifications/create/compose', NotificationsController.composeCreate);
-app.get('/notifications/create/templates', NotificationsController.templates);
-app.post('/notifications/create', NotificationsController.create);
-app.get('/notifications/:notification/edit', NotificationsController.edit);
-app.post('/notifications/:notification/edit', NotificationsController.update);
-app.get('/notifications/:notification/remove', NotificationsController.remove);
+// app.get('/notifications', NotificationsController.index);
+// app.get('/notifications/create', NotificationsController.new);
+// app.get('/notifications/create/compose', NotificationsController.compose);
+// app.post('/notifications/create/compose', NotificationsController.composeCreate);
+// app.get('/notifications/create/templates', NotificationsController.templates);
+// app.post('/notifications/create', NotificationsController.create);
+// app.get('/notifications/:notification/edit', NotificationsController.edit);
+// app.post('/notifications/:notification/edit', NotificationsController.update);
+// app.get('/notifications/:notification/remove', NotificationsController.remove);
 
-app.get('/templates', TemplatesController.index);
-app.get('/templates/create', TemplatesController.new);
-app.post('/templates/create', TemplatesController.create);
-app.get('/templates/remove/:template', TemplatesController.destroy);
-app.get('/templates/edit/:template', TemplatesController.edit);
-app.post('/templates/edit/:template', TemplatesController.update);
+// app.get('/templates', TemplatesController.index);
+// app.get('/templates/create', TemplatesController.new);
+// app.post('/templates/create', TemplatesController.create);
+// app.get('/templates/remove/:template', TemplatesController.destroy);
+// app.get('/templates/edit/:template', TemplatesController.edit);
+// app.post('/templates/edit/:template', TemplatesController.update);
 
-app.get('/groups', GroupsController.index);
-app.get('/groups/create', GroupsController.new);
-app.post('/groups/create', GroupsController.create);
-app.get('/groups/edit/:group', GroupsController.edit);
-app.post('/groups/edit/:group', GroupsController.update);
-app.get('/groups/remove/:group', GroupsController.destroy);
-app.get('/groups/activate/:group', GroupsController.activate);
-app.get('/groups/address/:group', GroupsController.address);
-app.post('/groups/address/:group', GroupsController.addressUpdate);
+// app.get('/groups', GroupsController.index);
+// app.get('/groups/create', GroupsController.new);
+// app.post('/groups/create', GroupsController.create);
+// app.get('/groups/edit/:group', GroupsController.edit);
+// app.post('/groups/edit/:group', GroupsController.update);
+// app.get('/groups/remove/:group', GroupsController.destroy);
+// app.get('/groups/activate/:group', GroupsController.activate);
+// app.get('/groups/address/:group', GroupsController.address);
+// app.post('/groups/address/:group', GroupsController.addressUpdate);
 
-app.get('/clients', ClientsController.index);
-app.get('/clients/create', ClientsController.new);
-app.post('/clients/create', ClientsController.create);
+// app.get('/clients', ClientsController.index);
+// app.get('/clients/create', ClientsController.new);
+// app.post('/clients/create', ClientsController.create);
 
-app.get('/clients/:client', ClientsController.clientCard);
-app.get('/clients/:client/edit', ClientsController.edit);
-app.post('/clients/:client/edit', ClientsController.update);
-app.get('/clients/:client/alter/:status', ClientsController.alter);
-app.get('/clients/:client/closeoutsurvey', ClientsController.viewCloseoutSurvey);
-app.post('/clients/:client/closeoutsurvey', ClientsController.submitCloseoutSurvey);
-app.get('/clients/:client/transfer', ClientsController.transferSelect);
-app.post('/clients/:client/transfer', ClientsController.transferSubmit);
+// app.get('/clients/:client', ClientsController.clientCard);
+// app.get('/clients/:client/edit', ClientsController.edit);
+// app.post('/clients/:client/edit', ClientsController.update);
+// app.get('/clients/:client/alter/:status', ClientsController.alter);
+// app.get('/clients/:client/closeoutsurvey', ClientsController.viewCloseoutSurvey);
+// app.post('/clients/:client/closeoutsurvey', ClientsController.submitCloseoutSurvey);
+// app.get('/clients/:client/transfer', ClientsController.transferSelect);
+// app.post('/clients/:client/transfer', ClientsController.transferSubmit);
 
-app.get('/clients/:client/address', ClientsController.addressCraft);
-app.get('/clients/:client/address/templates', ClientsController.templates);
-app.post('/clients/:client/address', ClientsController.addressSubmit);
-app.get('/clients/:client/mediamessage', ClientsController.mediaAttachment);
-app.get('/clients/:client/voicemessage', VoiceController.new);
-app.post('/clients/:client/voicemessage', VoiceController.create);
+// app.get('/clients/:client/address', ClientsController.addressCraft);
+// app.get('/clients/:client/address/templates', ClientsController.templates);
+// app.post('/clients/:client/address', ClientsController.addressSubmit);
+// app.get('/clients/:client/mediamessage', ClientsController.mediaAttachment);
+// app.get('/clients/:client/voicemessage', VoiceController.new);
+// app.post('/clients/:client/voicemessage', VoiceController.create);
 
-app.get('/clients/:client/transcript', ClientsController.transcript);
-app.get('/clients/:client/messages', ClientsController.messagesIndex);
-app.post('/clients/:client/messages', ClientsController.messagesSubmit);
+// app.get('/clients/:client/transcript', ClientsController.transcript);
+// app.get('/clients/:client/messages', ClientsController.messagesIndex);
+// app.post('/clients/:client/messages', ClientsController.messagesSubmit);
 
-app.get('/clients/:client/edit/color', ColorsController.select);
-app.post('/clients/:client/edit/color', ColorsController.attribute);
+// app.get('/clients/:client/edit/color', ColorsController.select);
+// app.post('/clients/:client/edit/color', ColorsController.attribute);
 
-app.get('/clients/:client/conversations/:conversation/claim', ConversationsController.claimOption);
-app.post('/clients/:client/conversations/:conversation/claim', ConversationsController.claim);
+// app.get('/clients/:client/conversations/:conversation/claim', ConversationsController.claimOption);
+// app.post('/clients/:client/conversations/:conversation/claim', ConversationsController.claim);
 
-app.get('/clients/:client/communications', CommunicationsController.index);
-app.get('/clients/:client/communications/create', CommunicationsController.new);
-app.post('/clients/:client/communications/create', CommunicationsController.create);
-app.get('/clients/:client/communications/:communication/edit', CommunicationsController.edit);
-app.get('/clients/:client/communications/:communication/remove', CommunicationsController.remove);
+// app.get('/clients/:client/communications', CommunicationsController.index);
+// app.get('/clients/:client/communications/create', CommunicationsController.new);
+// app.post('/clients/:client/communications/create', CommunicationsController.create);
+// app.get('/clients/:client/communications/:communication/edit', CommunicationsController.edit);
+// app.get('/clients/:client/communications/:communication/remove', CommunicationsController.remove);
 
-app.get('/clients/:client/notifications', NotificationsController.index);
+// app.get('/clients/:client/notifications', NotificationsController.index);
 
-app.get('/org', DashboardController.org);
+// app.get('/org', DashboardController.org);
 
-app.get('/org/users', UsersController.index);
-app.get('/org/users/create', UsersController.new);
-app.post('/org/users/create', UsersController.create);
-app.get('/org/users/create/check/:email', UsersController.check);
-app.get('/org/users/:targetUser/alter/:case', UsersController.alter);
-app.get('/org/users/:targetUser/edit', UsersController.edit);
-app.post('/org/users/:targetUser/edit', UsersController.update);
-app.get('/org/users/:targetUser/transfer', UsersController.transferIndex);
-app.post('/org/users/:targetUser/transfer', UsersController.transferUpdate);
+// app.get('/org/users', UsersController.index);
+// app.get('/org/users/create', UsersController.new);
+// app.post('/org/users/create', UsersController.create);
+// app.get('/org/users/create/check/:email', UsersController.check);
+// app.get('/org/users/:targetUser/alter/:case', UsersController.alter);
+// app.get('/org/users/:targetUser/edit', UsersController.edit);
+// app.post('/org/users/:targetUser/edit', UsersController.update);
+// app.get('/org/users/:targetUser/transfer', UsersController.transferIndex);
+// app.post('/org/users/:targetUser/transfer', UsersController.transferUpdate);
 
-app.get('/org/departments', DepartmentsController.index);
-app.get('/org/departments/create', DepartmentsController.new);
-app.post('/org/departments/create', DepartmentsController.create);
-app.get('/org/departments/:department/edit', DepartmentsController.edit);
-app.post('/org/departments/:department/edit', DepartmentsController.update);
-app.get('/org/departments/:department/supervisors', DepartmentsController.supervisorsIndex);
-app.post('/org/departments/:department/supervisors', DepartmentsController.supervisorsUpdate);
-app.get('/org/departments/:department/alter/:case', DepartmentsController.alter);
+// app.get('/org/departments', DepartmentsController.index);
+// app.get('/org/departments/create', DepartmentsController.new);
+// app.post('/org/departments/create', DepartmentsController.create);
+// app.get('/org/departments/:department/edit', DepartmentsController.edit);
+// app.post('/org/departments/:department/edit', DepartmentsController.update);
+// app.get('/org/departments/:department/supervisors', DepartmentsController.supervisorsIndex);
+// app.post('/org/departments/:department/supervisors', DepartmentsController.supervisorsUpdate);
+// app.get('/org/departments/:department/alter/:case', DepartmentsController.alter);
 
-app.get('/org/numbers', PhoneNumbers.index);
-app.get('/org/numbers/create', PhoneNumbers.new);
+// app.get('/org/numbers', PhoneNumbers.index);
+// app.get('/org/numbers/create', PhoneNumbers.new);
 
-app.get('/org/clients', ClientsController.index);
-app.get('/org/clients/create', ClientsController.new);
-app.post('/org/clients/create', ClientsController.create);
+// app.get('/org/clients', ClientsController.index);
+// app.get('/org/clients/create', ClientsController.new);
+// app.post('/org/clients/create', ClientsController.create);
 
-app.get('/org/clients/:client', ClientsController.clientCard);
-app.get('/org/clients/:client/address', ClientsController.addressCraft);
-app.get('/org/clients/:client/address/templates', ClientsController.templates);
-app.post('/org/clients/:client/address', ClientsController.addressSubmit);
-app.get('/org/clients/:client/voicemessage', VoiceController.new);
-app.post('/org/clients/:client/voicemessage', VoiceController.create);
+// app.get('/org/clients/:client', ClientsController.clientCard);
+// app.get('/org/clients/:client/address', ClientsController.addressCraft);
+// app.get('/org/clients/:client/address/templates', ClientsController.templates);
+// app.post('/org/clients/:client/address', ClientsController.addressSubmit);
+// app.get('/org/clients/:client/voicemessage', VoiceController.new);
+// app.post('/org/clients/:client/voicemessage', VoiceController.create);
 
-app.get('/org/clients/:client/edit', ClientsController.edit);
-app.get('/org/clients/:client/edit', ClientsController.update);
-app.get('/org/clients/:client/alter/:status', ClientsController.alter);
-app.get('/org/clients/:client/closeoutsurvey', ClientsController.viewCloseoutSurvey);
-app.post('/org/clients/:client/closeoutsurvey', ClientsController.submitCloseoutSurvey);
+// app.get('/org/clients/:client/edit', ClientsController.edit);
+// app.get('/org/clients/:client/edit', ClientsController.update);
+// app.get('/org/clients/:client/alter/:status', ClientsController.alter);
+// app.get('/org/clients/:client/closeoutsurvey', ClientsController.viewCloseoutSurvey);
+// app.post('/org/clients/:client/closeoutsurvey', ClientsController.submitCloseoutSurvey);
 
-app.get('/org/clients/:client/transfer', ClientsController.transferSelect);
-app.post('/org/clients/:client/transfer', ClientsController.transferSubmit);
+// app.get('/org/clients/:client/transfer', ClientsController.transferSelect);
+// app.post('/org/clients/:client/transfer', ClientsController.transferSubmit);
 
-app.get('/org/clients/:client/communications/create', CommunicationsController.new);
-app.post('/org/clients/:client/communications/create', CommunicationsController.create);
+// app.get('/org/clients/:client/communications/create', CommunicationsController.new);
+// app.post('/org/clients/:client/communications/create', CommunicationsController.create);
 
-app.get('/org/captured', CaptureBoardController.index);
-app.get('/org/captured/attach/:conversation', CaptureBoardController.attachUserIndex);
-app.post('/org/captured/attach/:conversation', CaptureBoardController.attachUserSelect);
-app.get('/org/captured/attach/:conversation/user/:user', CaptureBoardController.attachClientIndex);
-app.post('/org/captured/attach/:conversation/user/:user', CaptureBoardController.attachUpdate);
-app.get('/org/captured/remove/:conversation', CaptureBoardController.removeConfirm);
-app.post('/org/captured/remove/:conversation', CaptureBoardController.remove);
+// app.get('/org/captured', CaptureBoardController.index);
+// app.get('/org/captured/attach/:conversation', CaptureBoardController.attachUserIndex);
+// app.post('/org/captured/attach/:conversation', CaptureBoardController.attachUserSelect);
+// app.get('/org/captured/attach/:conversation/user/:user', CaptureBoardController.attachClientIndex);
+// app.post('/org/captured/attach/:conversation/user/:user', CaptureBoardController.attachUpdate);
+// app.get('/org/captured/remove/:conversation', CaptureBoardController.removeConfirm);
+// app.post('/org/captured/remove/:conversation', CaptureBoardController.remove);
 
-app.get('/org/alerts/create', AlertsController.new);
-app.post('/org/alerts/create', AlertsController.create);
+// app.get('/org/alerts/create', AlertsController.new);
+// app.post('/org/alerts/create', AlertsController.create);
 
-app.get('/settings', SettingsController.index);
-app.post('/settings', SettingsController.update);
+// app.get('/settings', SettingsController.index);
+// app.post('/settings', SettingsController.update);
 
 
 // Redundant catch all
 app.get('/*', (req, res) => {
-  res.notFound();
+  res.redirect("/");
 });
 
 // Scheduled operations
