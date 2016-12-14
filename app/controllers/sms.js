5e5373f78b78ac1fd54da408964c70e9414dc525
Thomas Apodaca <tma@angrymonkey.com>
2016-12-13 17:01:08 -0800

    validating Twilio requests (#297)
    * add Twilio validation
    
    * trying validateRequest instead of validateExpressRequest
    
    * req.body instead of POST
    
    * validateExpressRequest again, with new logs
    
    * no more logging

--- app/controllers/sms.js
+++ app/controllers/sms.js
@@ -1,3 +1,4 @@
+const twilio = require('twilio');
 const Communications = require('../models/communications');
 const Conversations = require('../models/conversations');
 const Departments = require('../models/departments');
@@ -6,6 +7,8 @@ const SentimentAnalysis = require('../models/sentiment');
 const Users = require('../models/users');
 
 const sms = require('../lib/sms');
+
+const credentials = require('../../credentials')
 
 module.exports = {
 
@@ -22,74 +25,83 @@ module.exports = {
     const MessageStatus = req.body.SmsStatus;
     const MessageSID = req.body.MessageSid;
 
-    // Log IBM Sensitivity measures
-    SentimentAnalysis.logIBMSentimentAnalysis(req.body);
-    
-    let communication, conversations, clients, messages;
-    Communications.getOrCreateFromValue(fromNumber, 'cell')
-    .then((resp) => {
-      communication = resp;
-      return sms.retrieveClients(toNumber, communication);
-    }).then((resp) => {
-      clients = resp;
-      return Conversations.retrieveByClientsAndCommunication(
-        clients, communication
-      );
-    }).then((resp) => {
-      conversations = resp;
-      const conversationIds = conversations.map((conversation) => {
-        return conversation.convid;
-      });
+    // validateRequest returns true if the request originated from Twilio
+    // TODO: is there a better way than explicitly setting the protocol to https?
+    if (twilio.validateExpressRequest(req, credentials.authToken, {'protocol': 'https'})) {
+      // Log IBM Sensitivity measures
+      SentimentAnalysis.logIBMSentimentAnalysis(req.body);
       
-      return Messages.insertIntoManyConversations(conversationIds,
-                                                  communication.commid,
-                                                  text,
-                                                  MessageSID,
-                                                  MessageStatus,
-                                                  toNumber);
-    }).then((resp) => {
-      messages = resp;
-
-      // out of office check
-      conversations.forEach((conversation) => {
-        Users.findById(conversation.cm)
-        .then((user) => {
-          if (user && user.is_away) {
-            Messages.getLatestNumber(user.cmid, conversation.client)
-            .then((commId) => {
-              const awayMessage = user.away_message || 'I am currently out of office. I will respond as soon as possible.';
-              return Messages.sendOne(commId, awayMessage, conversation);
-            }).then(() => { }).catch((error) => { console.log(error); });
-          }
+      let communication, conversations, clients, messages;
+      Communications.getOrCreateFromValue(fromNumber, 'cell')
+      .then((resp) => {
+        communication = resp;
+        return sms.retrieveClients(toNumber, communication);
+      }).then((resp) => {
+        clients = resp;
+        return Conversations.retrieveByClientsAndCommunication(
+          clients, communication
+        );
+      }).then((resp) => {
+        conversations = resp;
+        const conversationIds = conversations.map((conversation) => {
+          return conversation.convid;
         });
-      });      
+        
+        return Messages.insertIntoManyConversations(conversationIds,
+                                                    communication.commid,
+                                                    text,
+                                                    MessageSID,
+                                                    MessageStatus,
+                                                    toNumber);
+      }).then((resp) => {
+        messages = resp;
 
-      // determine if there is auto-response logic
-      conversations.forEach((conversation) => {
-        Messages.findByConversation(conversation)
-        .then((messages) => {
-          return Messages.determineIfAutoResponseShouldBeSent(conversation, messages);
-        }).then((resp) => {
-          const shouldSendResponse = resp.sendResponse;
-          if (shouldSendResponse) {
-            const commId = resp.sendValues.communicationId;
-            const conversationId = resp.sendValues.conversationId;
-            const messageContent = resp.sendValues.content;
-            const clientId = conversation.client;
-            Conversations.closeAllWithClientExcept(clientId, conversationId)
-            .then(() => {
-              return Messages.sendOne(commId, messageContent, conversation);
-            }).then(() => { }).catch((error) => { console.log(error); });
-          }
-        }).catch(res.error500);
+        // out of office check
+        conversations.forEach((conversation) => {
+          Users.findById(conversation.cm)
+          .then((user) => {
+            if (user && user.is_away) {
+              Messages.getLatestNumber(user.cmid, conversation.client)
+              .then((commId) => {
+                const awayMessage = user.away_message || 'I am currently out of office. I will respond as soon as possible.';
+                return Messages.sendOne(commId, awayMessage, conversation);
+              }).then(() => { }).catch((error) => { console.log(error); });
+            }
+          });
+        });      
 
-        req.logActivity.client(conversation.client);
-        req.logActivity.conversation(conversation.convid);
+        // determine if there is auto-response logic
+        conversations.forEach((conversation) => {
+          Messages.findByConversation(conversation)
+          .then((messages) => {
+            return Messages.determineIfAutoResponseShouldBeSent(conversation, messages);
+          }).then((resp) => {
+            const shouldSendResponse = resp.sendResponse;
+            if (shouldSendResponse) {
+              const commId = resp.sendValues.communicationId;
+              const conversationId = resp.sendValues.conversationId;
+              const messageContent = resp.sendValues.content;
+              const clientId = conversation.client;
+              Conversations.closeAllWithClientExcept(clientId, conversationId)
+              .then(() => {
+                return Messages.sendOne(commId, messageContent, conversation);
+              }).then(() => { }).catch((error) => { console.log(error); });
+            }
+          }).catch(res.error500);
+
+          req.logActivity.client(conversation.client);
+          req.logActivity.conversation(conversation.convid);
+        });
+
+        // Send a blank response
+        const twilioResponse = new twilio.TwimlResponse();
+        res.send(twilioResponse.toString());
       });
+    }
+    else {
+      res.status(403).send('You are not Twilio. Buzz off.');
+    }
 
-      // Send a blank response
-      res.send('<?xml version=\'1.0\' encoding=\'UTF-8\'?><Response></Response>');
-    });
   },
 
 };
