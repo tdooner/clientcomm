56e77380ef59c716a140de8ef722268b4f70faf6
Tom Dooner <tom.dooner@brigade.com>
2017-02-06 10:03:13 -0800

    Replace our eslint configuration with Airbnb's
    I think Airbnb's javascript rules are becoming an industry standard at
    this point; and they are certainly more extensive than our existing
    eslint configuration at least.
    
    I went through and checked that all rules we had enabled are configured
    the same way in Airbnb's ruleset -- they are. So this should be a bit
    more painful until we fix the possibly-new styling lints, but we can
    address that on a file-by-file basis.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,12 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "camelcase": ["error"],
-        "prefer-const": ["error"],
-        "comma-style": ["error", "last"],
-        "comma-dangle": ["error", "always"],
-        "quote-props": ["error", "as-needed"],
-        "no-var": ["error"],
-        "new-cap": ["error"],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "id-length": ["error"],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-    }
+  'env': {
+    'es6': true,
+    'node': true
+  },
+  'parserOptions': {
+    'sourceType': 'module'
+  },
+  'extends': [
+    'eslint-config-airbnb-base',
+  ],
 };
