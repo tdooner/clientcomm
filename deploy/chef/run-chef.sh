1725ed52f53f065b86bc6f0958db1923c4d8c568
Tom Dooner <tom.dooner@brigade.com>
2017-02-17 14:14:49 -0800

    Pass `deploy_base_url` to Chef as attribute
    This configuration variable should be passed to Chef as an attribute so
    that it can be used in configuration files. (It probably makes sense to
    pass all configuration values this way instead of creating a config file
    with terraform.)
    
    To do this, a JSON structure is output to /etc/chef/attributes.json on
    each server and then chef is configured to load attributes from that
    file in the /etc/chef/solo.rb config file.
    
    This also moves most of the chef logic into a bash function that is
    called for each web server.

--- deploy/chef/run-chef.sh
+++ deploy/chef/run-chef.sh
@@ -4,28 +4,50 @@
 # uploading necessary to run Chef. In a full-blown Chef deployment this is
 # handled by a Chef server, but here it is a lot simpler to just do by hand.
 set -euo pipefail
+
 terraform_dir=$(cd $(dirname $0)/..; pwd)
-ip=$(terraform output -state $terraform_dir/terraform.tfstate web_ip)
-SSH="ssh ubuntu@$ip"
 
 cd $(dirname $0)
-echo "Installing Chef on ${ip}..."
-$SSH 'if [ ! $(which chef-solo) ]; then curl -L https://www.chef.io/chef/install.sh | sudo bash; fi'
 
-echo "Installing /etc/chef/solo.rb"
-cookbooks_dir=/data/chef/cookbook
-$SSH /bin/bash -c "\"
-sudo mkdir -p /etc/chef/ ${cookbooks_dir};
-echo 'cookbook_path \\\"${cookbooks_dir}\\\"' | sudo tee /etc/chef/solo.rb >/dev/null
-\""
+run_node() {
+  ip=$1
+  SSH="ssh ubuntu@$ip"
 
-echo "Packaging cookbooks"
-berks package clientcomm.tar.gz
+  echo "Installing Chef on ${ip}..."
+  $SSH 'if [ ! $(which chef-solo) ]; then curl -L https://www.chef.io/chef/install.sh | sudo bash; fi'
 
-echo "Uploading cookbooks"
-$SSH sudo rm -rf "${cookbooks_dir}/*"
-cat clientcomm.tar.gz | $SSH sudo tar xz --strip-components=1 -C "${cookbooks_dir}"
-trap "rm -f clientcomm.tar.gz" EXIT
+  echo "Installing /etc/chef/solo.rb"
+  cookbooks_dir=/data/chef/cookbook
+  chef_attributes=$(cat <<JSON
+{
+  "clientcomm": {
+    "deploy_base_url": "${TF_VAR_deploy_base_url}"
+  }
+}
+JSON
+)
 
-echo "Running chef..."
-$SSH 'sudo chef-solo --config /etc/chef/solo.rb -o "recipe[clientcomm]"'
+  $SSH /bin/bash -c "\"
+  sudo mkdir -p /etc/chef/ ${cookbooks_dir};
+  echo '
+    cookbook_path \\\"${cookbooks_dir}\\\"
+    json_attribs \\\"/etc/chef/attributes.json\\\"
+  ' | sudo tee /etc/chef/solo.rb >/dev/null
+  \""
+  echo $chef_attributes | $SSH sudo tee /etc/chef/attributes.json >/dev/null
+
+  echo "Packaging cookbooks"
+  berks package clientcomm.tar.gz
+
+  echo "Uploading cookbooks"
+  $SSH sudo rm -rf "${cookbooks_dir}/*"
+  cat clientcomm.tar.gz | $SSH sudo tar xz --strip-components=1 -C "${cookbooks_dir}"
+  trap "rm -f clientcomm.tar.gz" EXIT
+
+  echo "Running chef..."
+  $SSH 'sudo chef-solo --config /etc/chef/solo.rb -o "recipe[clientcomm]"'
+}
+
+for IP in $(terraform output -json -state $terraform_dir/terraform.tfstate web_ip | jq -r '.value[]'); do
+  run_node "$IP"
+done
