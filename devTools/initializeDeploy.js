9f1d1ccc43d9b8c928c0f286e9ead415453a26d5
Tom Dooner <tom.dooner@brigade.com>
2017-02-16 16:30:05 -0800

    Add initialization script to create me as the first user
    This adds an initialization script that creates an org, a department,
    and a user for a new deployment of the code.

---
+++ devTools/initializeDeploy.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+// usage: ./devTools/initializeDeploy.js --multnomah
+const db = require('../app/db');
+const readline = require('readline');
+const credentials = require('../credentials');
+const hashPw = require('../app/lib/pass').hashPw;
+
+const ask = (prompt, callback) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question(prompt, (answer) => {
+    callback(answer);
+    rl.close();
+  });
+};
+
+let ORG_NAME, ORG_EMAIL, ORG_TZ, DEPT_NAME, CFA_USERNAME, CFA_FIRST_NAME,
+  CFA_LAST_NAME;
+
+if (process.argv[2] === '--multnomah') {
+  ORG_NAME = 'Multnomah County';
+  ORG_EMAIL = 'tdooner@codeforamerica.org';
+  ORG_TZ = 'America/Los_Angeles';
+  DEPT_NAME = 'Department of Community Justice';
+  CFA_USERNAME = 'tdooner@codeforamerica.org';
+  CFA_FIRST_NAME = 'Tom';
+  CFA_LAST_NAME = 'Dooner';
+} else {
+  throw new Error('No deploy information set! Call this script with an argument.');
+}
+
+// create an organization
+const org = {
+  name: ORG_NAME,
+  phone: 1,
+  email: ORG_EMAIL,
+  expiration: '2100-01-01 00:00:00+00',
+  allotment: 10,
+  created: new Date(),
+  tz: ORG_TZ,
+};
+
+// create a department
+const department = {
+  organization: 1,
+  name: DEPT_NAME,
+  phone_number: 1,
+  created_by: 1,
+  active: true,
+};
+
+// create a superuser
+const owner = {
+  org: 1,
+  first: CFA_FIRST_NAME,
+  last: CFA_LAST_NAME,
+  email: CFA_USERNAME,
+  position: 'Officer',
+  admin: true,
+  active: true,
+  superuser: true,
+  class: 'owner',
+};
+
+// phone number
+const phoneNumber = {
+  value: credentials.twilioNum,
+  organization: 1,
+};
+
+ask(`Login Password for ${CFA_USERNAME}? `, (pw) => {
+  owner.pass = hashPw(pw);
+
+  // these need to be inserted in this order in order to satisfy foreign key
+  // constraints
+  db('orgs').insert(org)
+    .then(() => db('cms').insert(owner))
+    .then(() => db('phone_numbers').insert(phoneNumber))
+    .then(() => db('departments').insert(department))
+    .then(() => {
+      console.log('Created Org, Department, and User!')
+      process.exit(0);
+    })
+    .catch(err => {
+      console.error(err);
+      process.exit(1)
+    });
+});
