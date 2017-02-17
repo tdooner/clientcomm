5b167e23d699c6a3c6c3a0297313be245159c47f
Tom Dooner <tom.dooner@brigade.com>
2017-02-17 14:21:16 -0800

    Move systemd into clientcomm recipe
    There is a dependency here because the service needs to be started after
    the `npm install` command is run. Keeping the systemd_service resource
    in a different file means that later that dependency might be forgotten
    and mistakenly reordered.

--- deploy/chef/cookbook/recipes/_clientcomm.rb
+++ deploy/chef/cookbook/recipes/_clientcomm.rb
@@ -27,3 +27,21 @@ execute 'npm install' do
   )
   action :nothing
 end
+
+systemd_service 'clientcomm' do
+  description 'Clientcomm web process'
+  after %w[network.target]
+  install do
+    wanted_by 'multi-user.target'
+  end
+  service do
+    environment_file '/etc/clientcomm.conf'
+    exec_start '/usr/local/bin/node app/server.js'
+    working_directory '/home/clientcomm/clientcomm'
+    user 'clientcomm'
+    group 'clientcomm'
+  end
+
+  # This recipe should be included after all the clientcomm setup.
+  action [:create, :enable, :start]
+end
