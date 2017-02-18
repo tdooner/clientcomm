2e500625dcb9c3b173c89b8e834c2f50f80b4d24
Tom Dooner <tom.dooner@brigade.com>
2017-02-17 16:03:45 -0800

    Update activation alert email to use mailgun
    This method can be easily converted to use the mailgun methods as well.

--- app/lib/emailer.js
+++ app/lib/emailer.js
@@ -1,15 +1,11 @@
-const nodemailer = require('nodemailer');
 const colors = require('colors');
 
 // Secret stuff
 const credentials = require('../../credentials');
-const empw = credentials.em.password;
+const mailgun = require('./mailgun');
 
 // Only send to junk email when in development environment
 const CCENV = process.env.CCENV || 'development';
-
-// Create reusable transporter object using the default SMTP transport
-const transporter = nodemailer.createTransport(`smtps://clientcomm%40codeforamerica.org:${empw}@smtp.gmail.com`);
 
 module.exports = {
 
@@ -18,18 +14,13 @@ module.exports = {
                 }\n You can log on to your ClientComm account by going to www.clientcomm.org and, ` +
                 'from the login screen, you can choose to reset your password.';
 
-    const mailOptions = {
-      from: '"ClientComm - CJS" <clientcomm@codeforamerica.org>',
-      to: CCENV === 'development' ? 'clientcomm@codeforamerica.org' : email,
-      subject: 'CientComm - Welcome to ClientComm!',
-      text,
-      html: text,
-    };
-
-    if (CCENV == 'production') {
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) console.log(error);
-      });
+    if (CCENV === 'production') {
+      return mailgun.sendEmail(
+        CCENV === 'development' ? 'clientcomm@codeforamerica.org' : email,
+        '"ClientComm - CJS" <clientcomm@codeforamerica.org>',
+        'ClientComm - Welcome to ClientComm!',
+        html
+      );
     } else {
       console.log(`\tActivation alert email would have been sent to ${email}.`);
     }
