3cc65f58d4b05e8143b40ba1e80007b79e85c898
Tom Dooner <tom.dooner@brigade.com>
2017-02-24 11:48:44 -0800

    Fix "sudo: unable to resolve host ip-10-0-0-132" warning
    This is because we don't have local DNS set up to resolve the hostnames
    of these servers. We can patch this over by adding the hostname to the
    /etc/hosts file. This could present weirdness if we use these hostnames
    to communicate between machines and the hostnames change, but I can't
    see us doing that at any time soon.

--- deploy/chef/cookbook/recipes/default.rb
+++ deploy/chef/cookbook/recipes/default.rb
@@ -3,3 +3,9 @@ include_recipe 'clientcomm::_swap'
 include_recipe 'clientcomm::_nginx'
 include_recipe 'clientcomm::_nodejs'
 include_recipe 'clientcomm::_clientcomm'
+
+# fix 'sudo: unable to resolve host ip-10-0-0-132' error
+execute 'fix sudo hostname bug' do
+  command "echo '127.0.0.1 #{node['hostname']}' | sudo tee -a /etc/hosts"
+  not_if "grep '127.0.0.1 #{node['hostname']}' /etc/hosts"
+end
